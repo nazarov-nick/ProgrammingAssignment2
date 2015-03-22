21fd4f1c0585c13044df8ee70f66a61a7bc739c5
nazarov-nick <nf.nazarov@ya.ru>
2015-03-23 03:06:33 +0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R function is able to cache potentially time-consuming computations
+## functions below inverse matrix, it may be helpfull if matrix has a large size
 
-## Write a short comment describing this function
+##  function makeCacheMatrix() Creates an object that is a matrix 
+## with a set of options
 
 makeCacheMatrix <- function(x = matrix()) {
+ 
+  InvM <- NULL # initialization
+ 
+  # change matrix  'x'
+  set <- function(y) {
+    x <<- y
+    InvM <<- NULL
+  }
+  
+  #return a matrix  'x'
+  get <- function() x
+  
+  # change inversed  'x'
+  setInversion <- function(InvMatrix) InvM <<- InvMatrix
+  
+  #return a matrix that is the inverse of matrix  'x'
+  getInversion <- function() InvM
+  
+  #return a list of options
+  list(set = set, get = get,
+       setInversion = setInversion,
+       getInversion = getInversion)
 
 }
 
 
-## Write a short comment describing this function
+## function cacheSolveb () takes as argument an object makeCacheMatrix and return 
+## inverse of matrix that was contained in makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  InvM <- x$getInversion()
+ 
+  if(!is.null(InvM)) {
+    message("getting cached data")
+    return(InvM)
+  }
+ 
+  #inverse of matrix  'x' for the first time
+  data <- x$get()
+  InvM <- solve(data)
+  x$setInversion(InvM)
+  InvM
+   
 }
